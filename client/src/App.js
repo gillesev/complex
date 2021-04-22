bb8e410dac6410aec244a5ac56daa6040c4bf082
Gilles Eveloy <gillese@magenic.com>
2021-04-21 18:47:53 -0700

    Unit test failed when removed the anchor with text Learn React! added a h1 title above instead.

--- client/src/App.js
+++ client/src/App.js
@@ -11,13 +11,14 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <h1>Fibonnacci Calculator</h1>
           <a
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Fibonnacci Calculator
+            Learn React
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
